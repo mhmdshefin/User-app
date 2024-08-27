4ccc2050baac31f4400acab9089e49da05a84749
mhmd shefin <mshefin2005@gmail.com>
2024-08-27 22:21:13 +0530

    user app

---
+++ src/components/AddUsers.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddUsers = () => {
+  return (
+    <div>
+      <Navbar/>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">First name</label>
+                <input type="text" className="form-control" placeholder='enter first name' />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Last name</label>
+                <input type="text" className="form-control" placeholder='enter last name'/>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+               <label htmlFor="" className="form-label">Profile pic</label>
+               <input type="file" name="" id="" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Email ID</label>
+                <input type="text" className="form-control" placeholder='enter email' />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <button className="btn btn-success">submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddUsers
---
+++ src/components/SearchUsers.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Navbar from './Navbar'
+
+const SearchUsers = () => {
+  return (
+    <div>
+      <Navbar/>
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">User name</label>
+                <input type="text" className="form-control" placeholder='enter user name'/>
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">Search</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchUsers
